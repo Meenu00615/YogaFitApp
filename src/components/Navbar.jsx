8efd7a1f13363b29f9d55daaf0d8f3c9cfc05a73
Meenu Patel <149779716+Meenu00615@users.noreply.github.com>
2025-02-23 00:16:42 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className="navbar">
       <h1 className="logo">YogaFit</h1>
-      <ul className="nav-links">
+
+      <ul className={`nav-links ${menuOpen ? "active" : ""}`}>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/tutorials">Tutorials</Link></li>
         <li><Link to="/about">About Us</Link></li> 
